fbee3ac84923c2ce342e229fa9ed686f922cedc6
agus-darmawan <wayanagus.dr@gmail.com>
2025-04-12 13:05:20 +0700

    feat: add react router dom

---
+++ src/pages/home-page/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+const HomePage: FC = () => {
+  return (
+    <main>
+      <h1 className="text-blue-400">Welcome to the Home Page</h1>
+      <p>This is a simple home page component.</p>
+      <p>Feel free to customize it as needed!</p>
+      <p>Happy coding!</p>
+    </main>
+  );
+};
+
+export default HomePage;
